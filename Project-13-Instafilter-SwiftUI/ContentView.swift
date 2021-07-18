0763fddd25618aadc4e00e94372819d2264f0b0d
Baris <karalarbaris@gmail.com>
2021-07-18 09:34:48 +0200

    Initial Commit

---
+++ Project-13-Instafilter-SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Project-13-Instafilter-SwiftUI
+//
+//  Created by Baris Karalar on 18.07.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Project-13-Instafilter-SwiftUI/Project_13_Instafilter_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Project_13_Instafilter_SwiftUIApp.swift
+//  Project-13-Instafilter-SwiftUI
+//
+//  Created by Baris Karalar on 18.07.2021.
+//
+
+import SwiftUI
+
+@main
+struct Project_13_Instafilter_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
