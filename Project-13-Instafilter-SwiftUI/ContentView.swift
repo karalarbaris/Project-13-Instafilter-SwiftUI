f2c8428f0baa841f34e4b8b1f627aa18050b3fc5
Baris <karalarbaris@gmail.com>
2021-07-20 07:08:00 +0200

    Building our basic UI

--- Project-13-Instafilter-SwiftUI/ContentView.swift
+++ Project-13-Instafilter-SwiftUI/ContentView.swift
@@ -7,54 +7,120 @@
 
 import SwiftUI
 
-//How to save images to the user’s photo library
-//Using coordinators to manage SwiftUI view controllers
-//Wrapping a UIViewController in a SwiftUI view
+//Building our basic UI
 
 struct ContentView: View {
     
     @State private var image: Image?
-    @State private var showingImagePicker = false
-    
-    @State private var inputImage: UIImage?
+    @State private var filterIntensity = 0.5
     
     var body: some View {
         
-        VStack {
-            image?
-                .resizable()
-                .scaledToFit()
-            
-            Button("Select Image") {
-                showingImagePicker = true
+        
+        NavigationView {
+            VStack {
+                ZStack {
+                    Rectangle()
+                        .fill(Color.secondary)
+                    
+                    //display the image
+                    if image != nil {
+                        image?
+                            .resizable()
+                            .scaledToFit()
+                    } else {
+                        Text("Tap to select a picture")
+                            .foregroundColor(.white)
+                            .font(.headline)
+                    }
+                    
+                }
+                .onTapGesture {
+                    //select an image
+                }
+                
+                HStack {
+                    Text("Intensity")
+                    Slider(value: $filterIntensity)
+                }
+                .padding()
+                
+                HStack {
+                    Button("Change Filter") {
+                        //change filter
+                    }
+                    
+                    Spacer()
+                    
+                    Button("Save") {
+                        //save the picture
+                    }
+                }
+                
             }
+            .padding([.horizontal, .bottom])
+            .navigationTitle("App Name")
         }
-        .sheet(isPresented: $showingImagePicker, onDismiss: loadImage) {
-            ImagePicker(image: $inputImage)
-        }
-        
-    }
-    
-    func loadImage() {
-        guard let inputImage = inputImage else { return }
-        image = Image(uiImage: inputImage)
-
-        let imageSaver = ImageSaver()
-        imageSaver.writeToPhotoAlbum(image: inputImage)
         
     }
     
 }
 
-class ImageSaver: NSObject {
-    func writeToPhotoAlbum(image: UIImage) {
-        UIImageWriteToSavedPhotosAlbum(image, self, #selector(saveError), nil)
-    }
-    
-    @objc func saveError(_ image: UIImage, didFinishSavingWithError error: Error?, contextInfo: UnsafeRawPointer) {
-        print("Save finished")
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
     }
 }
+
+
+////How to save images to the user’s photo library
+////Using coordinators to manage SwiftUI view controllers
+////Wrapping a UIViewController in a SwiftUI view
+//
+//struct ContentView: View {
+//
+//    @State private var image: Image?
+//    @State private var showingImagePicker = false
+//
+//    @State private var inputImage: UIImage?
+//
+//    var body: some View {
+//
+//        VStack {
+//            image?
+//                .resizable()
+//                .scaledToFit()
+//
+//            Button("Select Image") {
+//                showingImagePicker = true
+//            }
+//        }
+//        .sheet(isPresented: $showingImagePicker, onDismiss: loadImage) {
+//            ImagePicker(image: $inputImage)
+//        }
+//
+//    }
+//
+//    func loadImage() {
+//        guard let inputImage = inputImage else { return }
+//        image = Image(uiImage: inputImage)
+//
+//        let imageSaver = ImageSaver()
+//        imageSaver.writeToPhotoAlbum(image: inputImage)
+//
+//    }
+//
+//}
+//
+//class ImageSaver: NSObject {
+//    func writeToPhotoAlbum(image: UIImage) {
+//        UIImageWriteToSavedPhotosAlbum(image, self, #selector(saveError), nil)
+//    }
+//
+//    @objc func saveError(_ image: UIImage, didFinishSavingWithError error: Error?, contextInfo: UnsafeRawPointer) {
+//        print("Save finished")
+//    }
+//}
 
 ////Showing multiple options with ActionSheet
 //
@@ -117,8 +183,3 @@ class ImageSaver: NSObject {
 //    }
 //}
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
-}
